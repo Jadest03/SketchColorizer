2ab9a3195eaffb7672aaae18b8104f815bf5f365
Mingyu <jadest1018@gmail.com>
2026-03-17 23:15:08 +0900

    Update: change batch norm -> group norm in unet.py

--- unet.py
+++ unet.py
@@ -26,10 +26,10 @@ class DoubleConv(nn.Module):
             
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
+            nn.GroupNorm(8, mid_channels), 
             nn.GELU(),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+            nn.GroupNorm(8, out_channels), 
             nn.GELU()
         )
         
